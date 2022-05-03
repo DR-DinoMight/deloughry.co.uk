9221fb344d74ff30251a06ef85fd6e78cca8efe7
Matthew Deloughry <matt@deloughry.co.uk>
2022-05-03 21:35:35 +0100

    added npm package

--- web/pages/api/discover-webmentions.ts
+++ web/pages/api/discover-webmentions.ts
@@ -1,7 +1,9 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
-import type { WebmentionOptions } from '../../lib/webmention-discovery';
-import { webmentionDiscovery } from '../../lib/webmention-discovery';
+
+import {webmentionDiscovery} from 'webmention-rss-discovery-sender';
+import type {WebmentionOptions} from 'webmention-rss-discovery-sender';
+
 
 export default async function handler(
   req: NextApiRequest,
