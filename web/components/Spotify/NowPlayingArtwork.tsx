0b1dc551a5901b5e9a5e12d4da3f51d9b1b1f278
Matthew Peck-Deloughry <matt@deloughry.co.uk>
2022-07-18 21:32:34 +0100

    tweaks

--- web/components/Spotify/NowPlayingArtwork.tsx
+++ web/components/Spotify/NowPlayingArtwork.tsx
@@ -2,22 +2,36 @@ import useSWR from 'swr';
 import {NowPlayingSong} from "../../lib/spotify";
 import Image from "next/future/image";
 import {fetcher} from "../../lib/fetcher";
+import { useEffect, useState } from 'react';
 
 
 export default function NowPlayingArtwork() {
   const {data} = useSWR<NowPlayingSong>('/api/spotify/now-playing', fetcher, { refreshInterval: 5000 });
 
+  const [artwork, useArtwork ] = useState();
+
+  //cache the artwork in state  so we can still display it when no longer playing
+
+  useEffect(() => {
+    if (data && data.isPlaying) {
+      useArtwork(data?.song?.albumArt[0]?.url)
+    }
+    }, [data]);
+
+
   return (
     <div className="w-full h-screen relative overflow-hidden m-auto">
-        {data?.isPlaying && (
-          <img src={data?.song?.albumArt[0]?.url} alt={data?.song?.name} className="m-auto border-white border-8 rounded-full animate-spinslow row-span-full col-span-full object-fill h-full aspect-square"/>
-        )}
-        <div className='bg-black/20 absolute top-1/2'>
-          <div className=" shadow-black drop-shadow-lg  rounded text-8xl  animate-marquee whitespace-nowrap">
-            Now Playing: {data?.song?.name} - {data?.song?.artist}
-          </div>
-        </div>
+      <div className={`${data?.isPlaying ? 'animate-spinslow' : ''} w-screen h-screen relative`}>
+        <img src={artwork} className={`m-auto border-white border-8 rounded-full row-span-full col-span-full object-fill h-full aspect-square object-fill`}/>
 
+      </div>
+        {data?.isPlaying && (
+          <div className='bg-black/20 absolute top-1/2'>
+            <div className=" shadow-black drop-shadow-lg  rounded text-8xl  animate-marquee whitespace-nowrap">
+              Now Playing: {data?.song?.name} - {data?.song?.artist}
+            </div>
+          </div>
+        )}
     </div>
   );
 }
